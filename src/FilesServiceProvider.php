15c3fdc23fc993de9a44005f69668c536efd568f
Dem <dstaranenko@gmail.com>
2016-02-01 00:53:01 +0200

    Update FilesServiceProvider.php

--- src/FilesServiceProvider.php
+++ src/FilesServiceProvider.php
@@ -2,21 +2,8 @@
 
 use Illuminate\Support\ServiceProvider;
 
-
 class FilesServiceProvider extends ServiceProvider
 {
-
-/*
-	public function register()
-	{
-		$this->app->bind('files', function () {
-			#exit('just to verify laravel loads this file');
-			return new Files;
-			#return new FilesController();
-			//return new Files();
-		});
-	}
-	*/
 	public function register()
 	{
 		 $this->app->bind('FilesController', function () {
@@ -24,12 +11,7 @@ class FilesServiceProvider extends ServiceProvider
 		});
 	}
 
-
-
 	public function boot(){
 		require __DIR__ . '/routes.php';
-		//include __DIR__.'/../routes.php';
-		//require __DIR__ . '/Http/routes.php';
 	}
-
 }
