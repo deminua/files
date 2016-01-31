24f5fca7454b35bf3ffcfad7c945a54c24fefd91
deminua <dstaranenko@gmail.com>
2016-02-01 01:14:38 +0200

    2 commit

--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,8 @@
 <?php
  
- Route::get('/files-index', ['as' => 'files.index', 'uses' => 'FilesController@index']);
- 
+
 Route::group(['prefix' => 'files', 'namespace' => 'Deminua\Files\Http\Controllers'], function () {
  
 	Route::get('/test', ['as' => 'files.index', 'uses' => 'FilesController@index']);
  
 });
-
-/*
-Route::get('/xxx', function () {
-    return 'good test';
-});
-*/
